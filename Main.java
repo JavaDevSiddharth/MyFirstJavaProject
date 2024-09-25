534bacc822e32ee3663e78a50e3f79087d6c7137
Siddharth Jagdhane <jagdhnesiddharth@gmail.com>
2024-09-26 00:09:48 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // starting with sum code
+        System.out.print("The sum of given numbers is: ");
+        int num1= 45;
+        int num2= 18;
+        int num3= 7;
+        int sum = num1 + num2 +num3;
+
+        System.out.println(sum);
+        // below is the substraction code
+        System.out.print("the substraction of given numbers are; ");
+        int num4= 45;
+        int num5= 18;
+        int num6= 07;
+        int substract = num4- num5- num6;
+        System.out.println(substract);
+
+
+        //starting with multiplication example
+        System.out.print("multiplication answer of given numbers are : ");
+        int num7= 45;
+        int num8= 18;
+        int num9= 07;
+        int multiply= (num7 * num8 * num9);
+        System.out.println(multiply);
+        // getting dividing numbers
+
+        System.out.print("the answer of divide numbers is: ");
+        int num10= 45;
+        int num11= 18;
+        int num12= 7;
+        int divide = num10 / num11/num12;
+        System.out.println(divide);
+        // final code is run so thanks you for doing the first code starting the journey then best of luck ........
+
+    }
+}
+
+
+
+
+
+
+
